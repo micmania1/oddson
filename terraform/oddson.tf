140ff01feda9ae6a6a3c9f49a0a29a5716df3025
micmania1 <mstrong@silverstripe.com>
2020-03-21 13:14:19 +1300

    [terraform] added test lambda function

--- terraform/oddson.tf
+++ terraform/oddson.tf
@@ -42,7 +42,33 @@ resource "aws_apigatewayv2_api" "api" {
     description = "An API to change the lives of people for the better"
 }
 
-# resource "aws_lambda_function"
+resource "aws_iam_role" "api_function_role" {
+    name = "oddson_api_function_role"
+
+    assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_lambda_function" "create_oddson" {
+    function_name = "oddson_create"
+    filename = "../api/src/test.zip"
+    runtime = "nodejs12.x"
+    role = aws_iam_role.api_function_role.arn
+    handler = "index.js"
+}
 
 output "api_url" {
     value = aws_apigatewayv2_api.api.api_endpoint
