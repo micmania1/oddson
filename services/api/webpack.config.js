b632b4f32d6d2c7db82b19a85c63bda9345bd7b6
micmania1 <mstrong@silverstripe.com>
2020-04-05 14:59:40 +1200

    [api] zip up the build files with webpack

--- services/api/webpack.config.js
+++ services/api/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const NodemonPlugin = require('nodemon-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
 
 const distFolder = path.resolve(__dirname, 'build');
 
@@ -33,6 +34,8 @@ const webpackConfig = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+
+    // Allows us to run our application as a nodejs app locally
     new NodemonPlugin({
       ext: 'ts,js',
     }),
@@ -41,8 +44,22 @@ const webpackConfig = {
 
 if (process.env.NODE_ENV === 'development') {
   webpackConfig.mode = 'development';
-  webpackConfig.entry.app = './src/app.local.ts';
   webpackConfig.devtool = 'inline-source-map';
+
+  // app.local.ts returns our application without the aws-serverless-express
+  // wrapper.
+  webpackConfig.entry.app = './src/app.local.ts';
+}
+
+if (process.env.NODE_ENV !== 'development') {
+  /**
+   * Zip up the build files so that terraform has something to deploy to
+   * our lambda function.
+   */
+  const zipBuild = new ZipPlugin({
+    filename: 'app.zip',
+  });
+  webpackConfig.plugins.push(zipBuild);
 }
 
 module.exports = webpackConfig;
