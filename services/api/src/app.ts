5332f5fe80f3d235e9cf04520b2ec6f873758174
micmania1 <mstrong@silverstripe.com>
2020-04-19 17:33:56 +1200

    [api] added cors support to API gateway

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -5,25 +5,32 @@ import cors from 'cors';
 const app = express();
 const router = express.Router();
 
-router.use(cors());
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
 router.use(awsServerlessExpressMiddleware.eventContext())
 
-router.post('/new', (req, res) => {
+router.options('/new', cors(corsOptions));
+router.post('/new', cors(corsOptions), (req, res) => {
   const json = JSON.parse('{"id":"fe5d746a-e582-4cec-b2c8-d0c44e9108d1","challenge":"Do the thing","odds":0,"challenger":{"name":"Scott","number":0},"victim":{"name":"Scott","number":0},"status":"new"}');
   res.json(json);
 });
 
-router.post('/activate/:id', (req, res) => {
+router.options('/activate/:id', cors(corsOptions));
+router.post('/activate/:id', cors(corsOptions), (req, res) => {
   const json = JSON.parse('{"id":"fe5d746a-e582-4cec-b2c8-d0c44e9108d1","challenge":"Do the thing","odds":20,"challenger":{"name":"Challenger","number":0},"victim":{"name":"Victim","number":7},"status":"activated"}');
   res.json(json);
 });
 
-router.post('/complete', (req, res) => {
+router.options('/complete/:id', cors(corsOptions));
+router.post('/complete/:id', cors(corsOptions), (req, res) => {
   const json = JSON.parse('{"id":"fe5d746a-e582-4cec-b2c8-d0c44e9108d1","challenge":"Do the thing","odds":20,"challenger":{"name":"Challenger","number":0},"victim":{"name":"Victim","number":7},"status":"activated"}');
   res.json(json);
 });
 
-router.get('/check/:id', (req, res) => {
+router.options('/check/:id', cors(corsOptions));
+router.get('/check/:id', cors(corsOptions), (req, res) => {
   const json = JSON.parse('{"id":"fe5d746a-e582-4cec-b2c8-d0c44e9108d1","challenge":"Do the thing","odds":0,"challenger":{"name":"Challenger","number":0},"victim":{"name":"Victim","number":2},"status":"new"}');
   res.json(json);
 });
