7bd425e8cfa647c8eeb04f0ac371c007f7dc18b4
Jake Ovenden <jake.ovenden@xero.com>
2020-04-26 13:57:44 +1200

    Create challenge endpoint

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import awsServerlessExpressMiddleware from 'aws-serverless-express/middleware';
 import cors from 'cors';
 import DB from './db';
 
 const app = express();
 const router = express.Router();
+app.use(bodyParser.json())
 
 const corsOptions = {
   origin: '*',
@@ -13,9 +15,9 @@ const corsOptions = {
 router.use(awsServerlessExpressMiddleware.eventContext())
 
 router.options('/new', cors(corsOptions));
-router.post('/new', cors(corsOptions), (req, res) => {
-  const json = JSON.parse('{"id":"fe5d746a-e582-4cec-b2c8-d0c44e9108d1","challenge":"Do the thing","odds":0,"challenger":{"name":"Scott","number":0},"victim":{"name":"Scott","number":0},"status":"new"}');
-  res.json(json);
+router.post('/new', cors(corsOptions), async (req, res) => {
+  const dbResponse = await DB.createNewChallenge(req.body)
+  res.json(dbResponse);
 });
 
 router.options('/activate/:id', cors(corsOptions));
