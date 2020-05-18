2a932ec29cd05c24845142095832d1aa4966e3f1
micmania1 <mstrong@silverstripe.com>
2020-05-18 18:59:03 +1200

    [ci] debugging why ci isnt detecting a workspace

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,20 +2,23 @@
 set -e
 
 # Pre-deploy hook
-./pre_deploy.sh
+# ./pre_deploy.sh
 
 # Run terraform
 # CircleCI uses the TF_WORKSPACE env variable
 TF_VARS_FILE=$($TF_WORKSPACE || terraform workspace show)
 
+echo "WORKSPACE = ${TF_VARS_FILE}"
+
 if [ -f "${TF_VARS_FILE}.tfvars" ]
 then
   echo "Using var file: ${TF_VARS_FILE}.tfvars"
-  terraform apply -auto-approve -lock=true -input=false -var-file="${TF_VARS_FILE}.tfvars"
+  # terraform apply -auto-approve -lock=true -input=false -var-file="${TF_VARS_FILE}.tfvars"
 else
-  terraform apply -auto-approve -lock=true -input=false
+  echo "NO VAR FILE FOUND"
+  # terraform apply -auto-approve -lock=true -input=false
 fi
 
 
 # Post-deploy hook
-./post_deploy.sh
+# ./post_deploy.sh
