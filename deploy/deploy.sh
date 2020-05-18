1d6b139a1774c1f9fd35634e3448a6a5a6149903
micmania1 <mstrong@silverstripe.com>
2020-05-18 19:09:10 +1200

    [ci] removed important tfvars files from gitignore

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,23 +2,21 @@
 set -e
 
 # Pre-deploy hook
-# ./pre_deploy.sh
+./pre_deploy.sh
 
 # Run terraform
 # CircleCI uses the TF_WORKSPACE env variable
 TF_VARS_FILE=$($TF_WORKSPACE || terraform workspace show)
 
-echo "WORKSPACE = ${TF_VARS_FILE}"
-
 if [ -f "${TF_VARS_FILE}.tfvars" ]
 then
   echo "Using var file: ${TF_VARS_FILE}.tfvars"
-  # terraform apply -auto-approve -lock=true -input=false -var-file="${TF_VARS_FILE}.tfvars"
+  terraform apply -auto-approve -lock=true -input=false -var-file="${TF_VARS_FILE}.tfvars"
 else
-  echo "NO VAR FILE FOUND"
-  # terraform apply -auto-approve -lock=true -input=false
+  echo "No tfvars file found. Deploying with default variables."
+  terraform apply -auto-approve -lock=true -input=false
 fi
 
 
 # Post-deploy hook
-# ./post_deploy.sh
+./post_deploy.sh
---
+++ deploy/prod.tfvars
@@ -0,0 +1,9 @@
+environment_type = "prod"
+domain           = "oddson.dev"
+web_domains = [
+  "oddson.dev",
+  "www.oddson.dev"
+]
+api_domains = [
+  "api.oddson.dev"
+]
